314fef40efc9a10f1b6bc22eacd828a47453c492
Tom OMalley <tom@intrepid.io>
2016-05-10 09:38:15 -0400

    adds small mode for ArtistHeaderView on iPhones < 6

--- spotifyMini/ViewControllers/Artist/ArtistHeaderView.swift
+++ spotifyMini/ViewControllers/Artist/ArtistHeaderView.swift
@@ -11,20 +11,24 @@ import Kingfisher
 
 class ArtistHeaderView : UIView {
 
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var profileImageView: UIImageView!
-    @IBOutlet weak var headerImageView: UIImageView!
-    @IBOutlet weak var blurImageView: UIImageView!
-    @IBOutlet weak var blurContainerView: UIView!
-    @IBOutlet weak var blurHeightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var followButton: UIButton! {
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var profileImageView: UIImageView! {
+        didSet {
+            self.profileImageView.layer.cornerRadius = 60.0
+        }
+    }
+    @IBOutlet private weak var headerImageView: UIImageView!
+    @IBOutlet private weak var blurImageView: UIImageView!
+    @IBOutlet private weak var blurContainerView: UIView!
+    @IBOutlet private weak var followButton: UIButton! {
         didSet {
             self.followButton.layer.borderColor = UIColor.lightGrayColor().CGColor
             self.followButton.layer.borderWidth = 0.5
             self.followButton.layer.cornerRadius = self.followButton.ip_halfHeight
         }
     }
-    
+    @IBOutlet weak var blurHeightConstraint: NSLayoutConstraint!
+
     var artist: SPTArtist? {
         didSet {
             if let artist = self.artist {
@@ -42,6 +46,20 @@ class ArtistHeaderView : UIView {
     var blurAlpha: CGFloat = 1 {
         didSet {
             blurContainerView.alpha = blurAlpha
+        }
+    }
+
+    var headerHeight: CGFloat = ArtistHeaderHeightLarge {
+        didSet {
+            self.blurHeightConstraint.constant = self.headerHeight
+            if let heightConstraint = self.profileImageView.ip_heightConstraint {
+                if self.headerHeight == ArtistHeaderHeightSmall {
+                    heightConstraint.constant = 90.0
+                } else {
+                    heightConstraint.constant = 120
+                }
+                self.profileImageView.layer.cornerRadius = heightConstraint.constant / 2
+            }
         }
     }
 
